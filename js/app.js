be68b5bdd82594350b0c17c692c57505c0ace056
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-06-12 16:17:12 +0500

    index page

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ $('.product-slick').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     swipeToSlide: true,
-    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><img src="img/ic_p.svg" class="img-fluid"/></button>',
-    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><img src="img/ic_n.svg" class="img-fluid"/></button>',
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><img src="img/ic_prev.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><img src="img/ic_next.svg" class="img-fluid"/></button>',
     responsive: [{
     breakpoint: 1024, settings: {
         slidesToShow: 6, slidesToScroll: 1, infinite: true, dots: true
